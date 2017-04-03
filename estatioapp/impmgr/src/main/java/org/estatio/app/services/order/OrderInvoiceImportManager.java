34a25d001e93daae46b21d77254f0cdf155c9b43
Johan Doornenbal <johan@filternet.nl>
2017-04-03 16:22:39 +0200

    EST-1201: second pass

---
+++ estatioapp/impmgr/src/main/java/org/estatio/app/services/order/OrderInvoiceImportManager.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2012-2015 Eurocommercial Properties NV
+ *
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.estatio.app.services.order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.Collection;
+import org.apache.isis.applib.annotation.CollectionLayout;
+import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.Nature;
+import org.apache.isis.applib.annotation.Parameter;
+import org.apache.isis.applib.annotation.ParameterLayout;
+import org.apache.isis.applib.annotation.Publishing;
+import org.apache.isis.applib.annotation.RenderType;
+import org.apache.isis.applib.annotation.SemanticsOf;
+import org.apache.isis.applib.value.Blob;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@DomainObject(
+        nature = Nature.VIEW_MODEL,
+        objectType = "org.estatio.app.services.order.OrderInvoiceImportManager"
+)
+public class OrderInvoiceImportManager {
+
+    public OrderInvoiceImportManager() {
+    }
+
+    public OrderInvoiceImportManager(final Blob spreadSheet) {
+        this.spreadSheet = spreadSheet;
+    }
+
+    public OrderInvoiceImportManager(final OrderInvoiceImportManager manager){
+        this.spreadSheet = manager.getSpreadSheet();
+    }
+
+    public String title() {
+        return "Order - Invoice Import";
+    }
+
+    @Getter @Setter
+    private Blob spreadSheet;
+
+    @Action(publishing = Publishing.DISABLED, semantics = SemanticsOf.IDEMPOTENT)
+    public OrderInvoiceImportManager importExcelSheet(
+            @Parameter(fileAccept = ".xlsx")
+            @ParameterLayout(named = "Excel spreadsheet")
+            final Blob spreadsheet) {
+        setSpreadSheet(spreadsheet);
+        return this;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Collection()
+    @CollectionLayout(
+            render = RenderType.EAGERLY
+    )
+    public List<OrderInvoiceImportLine> getLines() {
+        return getSpreadSheet()!=null ? orderInvoiceImportService.createLines(getSpreadSheet()) : new ArrayList<>();
+    }
+
+    @Inject
+    private DomainObjectContainer container;
+
+    @Inject
+    private OrderInvoiceImportService orderInvoiceImportService;
+
+}
---
+++ estatioapp/impmgr/src/main/java/org/estatio/app/services/order/OrderInvoiceImportMenu.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2012-2015 Eurocommercial Properties NV
+ *
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.estatio.app.services.order;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.DomainServiceLayout;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.NatureOfService;
+import org.apache.isis.applib.annotation.SemanticsOf;
+import org.apache.isis.applib.value.Blob;
+
+@DomainService(
+        nature = NatureOfService.VIEW_MENU_ONLY,
+        objectType = "org.estatio.app.services.order.OrderInvoiceImportMenu"
+)
+@DomainServiceLayout(
+        menuBar = DomainServiceLayout.MenuBar.PRIMARY,
+        named = "Projects"
+)
+public class OrderInvoiceImportMenu {
+
+    @Action(semantics = SemanticsOf.IDEMPOTENT)
+    @MemberOrder(sequence = "1")
+    public OrderInvoiceImportManager importOrdersAndInvoicesNotWorking(final Blob spreadSheet) {
+        return new OrderInvoiceImportManager(spreadSheet);
+    }
+
+    @Action(semantics = SemanticsOf.IDEMPOTENT)
+    @MemberOrder(sequence = "1")
+    public List<OrderInvoiceImportLine> importOrdersAndInvoicesWorking(final Blob spreadSheet) {
+        return orderInvoiceImportService.createLines(spreadSheet);
+    }
+
+    @Inject
+    private OrderInvoiceImportService orderInvoiceImportService;
+
+}
